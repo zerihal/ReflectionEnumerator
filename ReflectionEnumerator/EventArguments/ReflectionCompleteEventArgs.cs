cd2716930f9cb306974b7a1c1f38c7c68e494cb7
John Sweet <John.Sweet@mail.nidec.com>
2024-12-02 12:00:44 +0000

    Added missing comments.

--- ReflectionEnumerator/EventArguments/ReflectionCompleteEventArgs.cs
+++ ReflectionEnumerator/EventArguments/ReflectionCompleteEventArgs.cs
@@ -1,9 +1,19 @@
 ﻿namespace ReflectionEnumerator.EventArguments
 {
+    /// <summary>
+    /// Reflection completed event arguments.
+    /// </summary>
     public class ReflectionCompleteEventArgs : EventArgs
     {
+        /// <summary>
+        /// Count of reflected assembly objects.
+        /// </summary>
         public int ReflectedAssemblyObjects { get; }
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="reflectedAssemblyObjects">Number of reflected assembly objects found.</param>
         public ReflectionCompleteEventArgs(int reflectedAssemblyObjects)
         {
             ReflectedAssemblyObjects = reflectedAssemblyObjects;
--- ReflectionEnumerator/Objects/SerializationType.cs
+++ ReflectionEnumerator/Objects/SerializationType.cs
@@ -1,8 +1,17 @@
 ﻿namespace ReflectionEnumerator.Objects
 {
+    /// <summary>
+    /// Serialization type for saving or exporting interrogated assembly
+    /// </summary>
     public enum SerializationType
     {
+        /// <summary>
+        /// JSON format.
+        /// </summary>
         JSON,
+        /// <summary>
+        /// XML format.
+        /// </summary>
         XML
     }
 }
